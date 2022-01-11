6a6fc02e13b9c5144d9e4534dc7a1eb8cac45d2d
cardansel <cardansel@gmail.com>
2022-01-11 02:12:25 -0300

    Creando las migraciones, y la rutas api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/comments', "App\Http\Controllers\Api\CommentController@index");

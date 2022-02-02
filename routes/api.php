638fc360e3f23ec3815180601c99d2368acfcb4c
Carlos Daniel selva <cardansel@gmail.com>
2022-02-01 21:07:05 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/save-comment', "App\Http\Controllers\Api\CommentController@store");
+Route::post('/comment', "App\Http\Controllers\Api\CommentController@store");
 
